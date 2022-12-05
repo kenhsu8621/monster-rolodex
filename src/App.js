1218aa2f8e8bfdc2ea63cd0cfd22056376f02830
kenhsu8621 <s988662211@gmail.com>
2022-12-05 23:12:34 +0800

    [feat] 1. Add Search input and card section 2. Use double quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.scss';
+import { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state);
+          }
+        ));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) => {
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          })
+
+          this.setState(() => {
+            return { monsters: filteredMonsters };
+          })
+        }} />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h3>{monster.name}</h3>
+              <div>{monster.email}</div>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
